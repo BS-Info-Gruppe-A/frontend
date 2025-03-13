ff9a9ee33f3d8b88ab6352f964d08d75b68ea284
michaelrittmeister <michael@rittmeister.in>
2025-03-13 10:32:25 +0100

    Add preloader to reduce time to first frame

--- web/src/commonMain/kotlin/Main.kt
+++ web/src/commonMain/kotlin/Main.kt
@@ -1,12 +1,15 @@
 package eu.bsinfo.web
 
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.CanvasBasedWindow
 import eu.bsinfo.BSInfoApp
+import kotlinx.browser.document
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     CanvasBasedWindow {
+        SideEffect { document.getElementById("loading-container")!!.remove() }
         BSInfoApp()
     }
 }
